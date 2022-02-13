3f476a936f78817e34eb3889843521f49c12df16
Christian Schäfer <14093936+LonelessCodes@users.noreply.github.com>
2022-02-13 23:25:13 +0100

    Delete TestProgramm

--- Vorlesungscode/TestProgramm/src/TestProgramm.java
+++
@@ -1,8 +0,0 @@
-/**
- * TestProgramm
- */
-public class TestProgramm {
-    public static void main(String[] args) {
-        System.out.println("Das habe ich ganz allein geschafft!");
-    }    
-}
