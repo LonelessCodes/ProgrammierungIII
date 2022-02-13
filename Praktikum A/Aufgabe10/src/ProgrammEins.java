04661993e10677071ebb660548551dc21802cb07
Christian Schäfer <14093936+LonelessCodes@users.noreply.github.com>
2022-02-13 23:25:30 +0100

    Add Aufgabe 10

---
+++ Praktikum A/Aufgabe10/src/ProgrammEins.java
@@ -0,0 +1,5 @@
+public class ProgrammEins {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Endlich ist es soweit! Mein erstes Programm läuft...");
+    }
+}
