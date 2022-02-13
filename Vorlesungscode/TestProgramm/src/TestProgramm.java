ae4101cbfb9d360f927836b04fbf5373b1fe347e
Christian Schäfer <14093936+LonelessCodes@users.noreply.github.com>
2022-02-13 21:45:27 +0100

    VL 02 Hello World

---
+++ Vorlesungscode/TestProgramm/src/TestProgramm.java
@@ -0,0 +1,8 @@
+/**
+ * TestProgramm
+ */
+public class TestProgramm {
+    public static void main(String[] args) {
+        System.out.println("Das habe ich ganz allein geschafft!");
+    }    
+}
