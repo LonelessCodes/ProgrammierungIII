cdf3717cc8db52360bfeb62ebfa201b38e1c46cc
Christian Schäfer <14093936+LonelessCodes@users.noreply.github.com>
2022-02-14 19:05:40 +0100

    Add Aufgabe C01

---
+++ Praktikum C/Aufgabe01.java
@@ -0,0 +1,8 @@
+public class Aufgabe01 {
+    public static void main(String[] args) {
+        int[] a = { 0, 2, 4, 9, 3, 7, 10, 69, Integer.MAX_VALUE, 420 };
+
+        for (int elem : a) System.out.print(elem + " ");
+        System.out.println();
+    }
+}
