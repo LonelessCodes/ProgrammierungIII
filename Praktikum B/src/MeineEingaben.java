fe1241a2a5d3119b6d27dacb8e5868da89cddfc9
Christian Schäfer <14093936+LonelessCodes@users.noreply.github.com>
2022-02-14 13:06:11 +0100

    Add Aufgabe B05

--- Praktikum B/src/MeineEingaben.java
+++ Praktikum B/src/MeineEingaben.java
@@ -3,5 +3,8 @@ public class MeineEingaben {
     public static void main(String[] args) {
         System.out.println("Eingabe 1: >" + args[0] + "< und");
         System.out.println("Eingabe 2: >" + args[1] + "<");
+
+        // Es passiert sind besonderes. Zusätzliche Parameter werden
+        // einfach nicht gelesen
     }
 }
