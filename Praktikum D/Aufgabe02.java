5948337a2b3b1ce0bb9affff798e532072d7d0a4
Christian Schäfer <14093936+LonelessCodes@users.noreply.github.com>
2022-02-15 17:24:59 +0100

    Anmerkung Aufgabe D02

--- Praktikum D/Aufgabe02.java
+++ Praktikum D/Aufgabe02.java
@@ -4,6 +4,9 @@ public class Aufgabe02 {
 
         for (int i = 0; i < 100000; i++) {
             c = c++;
+            // Java gibt falsche Warnung aus. Eigentlich ist c = c++; der
+            // Ausdruck, der keine Wirkung hat und d = ++d; hat eine Wirkung.
+            // Der Linter sagt aber, dass d = ++d; keine Wirkung hätte.
             d = ++d;
             System.out.printf("c: %d , d: %d\n", c, d);
         }
